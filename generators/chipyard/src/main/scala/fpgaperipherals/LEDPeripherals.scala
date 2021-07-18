7c9dab95c125f1d77a8d68bda7a36cda3f80c84d
Alexander Lukens <alexdlukens@gmail.com>
2021-07-18 14:28:51 -0500

    Added R G and B LEDs for LED0 on arty board

--- generators/chipyard/src/main/scala/fpgaperipherals/LEDPeripherals.scala
+++ generators/chipyard/src/main/scala/fpgaperipherals/LEDPeripherals.scala
@@ -13,14 +13,13 @@ import sifive.blocks.devices.gpio._
 import testchipip._
 
 case class LEDParams(
-  address: BigInt = 0x0,
   color: String = "red",
-  width: Int = 1,
-  header: String = "&gpio0 0",
-  rgb: Boolean = false,
-  number: Int = 0)
+  header: String = "&gpio0",
+  pinNumber: Int = 0,
+  label: String = "LD0",
+  rgb: Boolean = false)
 
-case object LEDKey extends Field[Option[LEDParams]](None)
+case object LEDKey extends Field[Seq[LEDParams]](Nil)
 
 
 object GPIOLEDs {
@@ -30,17 +29,19 @@ object GPIOLEDs {
 class GPIOLEDs(params: LEDParams)(implicit p: Parameters) extends LazyModule with BindingScope
 {
     val beatBytes = 8 
-    val device = new SimpleDevice({"led" + GPIOLEDs.index.toString}, Seq("sifive,gpio-leds")){
+    val device = new SimpleDevice({"led@" + GPIOLEDs.index.toString}, Seq("sifive,gpio-leds")){
+        GPIOLEDs.index = GPIOLEDs.index + 1
         def extraResources(resources: ResourceBindings) = Map(
-            "label"      -> Seq(ResourceString("LD0red")),
-            "gpios"          -> Seq(ResourceString("<&gpio0 0>")),
+            "label"      -> Seq(ResourceString(params.label + params.color)),
+            "gpios"          -> Seq(ResourceString("<&gpio0 "+params.pinNumber.toString + ">")),
+            "color"      -> Seq(ResourceString(params.color)),
             "linux,default-trigger" -> Seq(ResourceString("none")))
         override def describe(resources: ResourceBindings): Description = {
         val Description(name, mapping) = super.describe(resources)
         Description(name, mapping ++ extraResources(resources))
         }
         ResourceBinding {
-            Resource(this, "").bind(ResourceInt(0x5000))
+            Resource(this, "exists").bind(ResourceString("true"))
         }
 
         
@@ -77,27 +78,34 @@ class LEDTL2(params: LEDParams)(implicit p: Parameters) extends LazyModule with 
 
     
 }   
-    
-      
+// works for 1 LED
+
+// trait CanHavePeripheryGPIOLED {
+//     this: BaseSubsystem =>
+//     private val portName = "leds"
+
+//     val led = p(LEDKey) match {
+//         case Some(params) => {
+//             val led = LazyModule(new GPIOLEDs(params)(p))
+//             Some(led)
+//         }
+//         case None => None
+//     }
+// }      
 
 
 trait CanHavePeripheryGPIOLED {
     this: BaseSubsystem =>
-    private val portName = "leds"
 
-    val led = p(LEDKey) match {
-        case Some(params) => {
-            val led = LazyModule(new GPIOLEDs(params)(p))
-            Some(led)
-        }
-        case None => None
-    }
-}
-
+    val leds =Seq():+(p(LEDKey).foreach(params => {
+        val led = LazyModule(new GPIOLEDs(params)(p))
+        Some(led)
+    }))
+} 
 trait CanHavePeripheryGPIOLEDImp extends LazyModuleImp{
     val outer: CanHavePeripheryGPIOLED
 }
 
 class WithGPIOLEDs extends Config((site,here,up) => {
-    case LEDKey => Some(LEDParams())
+    case LEDKey => (Seq(LEDParams(pinNumber = 1), LEDParams(color="green", pinNumber = 2), LEDParams(color="blue", pinNumber = 3)))
 })
